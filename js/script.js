2e8af2e1eb57b06d5f9fa7e17b4435d3c8a5ab2c
1chooo <hugo970217@gmail.com>
2023-06-08 20:02:09 +0800

    add: page changed rule

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function changePage(pageName) {
+  if (pageName === 'Home') {
+    window.location.href = './index.html';
+  } else if (pageName === 'Ho') {
+    window.location.href = './ho/index.html';
+  } else if (pageName === 'example') {
+    window.location.href = './example/index.html';
+  }
+}
